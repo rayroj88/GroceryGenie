868ef8f9b78ae0f514c64567e5c18ef53f3469fc
Danny Sears <dannywsears@gmail.com>
2024-02-23 10:57:56 -0600

    Added login route and login logic not functional yet

--- app.py
+++ app.py
@@ -6,6 +6,27 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+    
+    # Logic once the landing page has been changed to the login
+    #if 'user_id' in session:
+        #return 'Logged in successfully!'
+    #lse:
+        #return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Placeholder for the basic authentication logic, this will need to be changed.
+        username = request.form['username']
+        password = request.form['password']
+        
+        # This is a placeholder, this will need to check against a databse in the future?
+        if username == "admin" and password == "password":
+            session['user_id'] = username
+            return redirect(url_for('home'))
+        else:
+            return 'Invalid credentials'
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
