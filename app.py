6b81df59289c9ca545df3da077795a36854a5fef
Danny Sears <dannywsears@gmail.com>
2024-02-24 15:58:14 -0600

    Added sqllite basic databse support

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import os
+from datetime import datetime
+from extensions import db
 
 #USE 'admin' AS USERNAME
 #USE 'password' AS PASSWORD
@@ -8,6 +10,20 @@ app = Flask(__name__)
 # Secret key needed?
 app.secret_key = os.urandom(24)
 
+# Config db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shopping_list.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+# Create all tables in db
+with app.app_context():
+    db.create_all()
+
+# Import models after db is defined
+from models import User, ShoppingList, Item
+
+# Homepage currently index.html
 @app.route('/')
 def home():
     #return render_template('index.html')
@@ -17,7 +33,7 @@ def home():
     else:
         return redirect(url_for('login'))  # Redirect to login page if not logged in
 
-
+# Login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -33,10 +49,30 @@ def login():
             return 'Invalid credentials'
     return render_template('login.html')
 
+# Logout route
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
     return redirect(url_for('login'))
+
+# Register page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Example: Retrieve username, email, and password from form data
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        new_user = User(username=username, email=email, password_hash=password, date_created=datetime.now())
+        
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+            return "User registered successfully!"
+        except Exception as e:
+            db.session.rollback()
+            return "Error: " + str(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
