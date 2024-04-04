2d3390bff5dd2c4d4d48750564bd732ab25edacf
Ray <xvc3@txstate.edu>
2024-04-04 11:11:16 -0500

    More CSS changes to get stuff displaying better. Deleted some redeclared variables.

--- routes/get_saved_lists.py
+++ routes/get_saved_lists.py
@@ -19,30 +19,6 @@ def get_saved_lists():
     conn.close()
 
     return jsonify({'saved_lists': saved_lists})
-from flask import Blueprint, jsonify
-import sqlite3
-
-saved_lists_bp = Blueprint('saved_lists_bp', __name__)
-@saved_lists_bp.route('/get_saved_lists', methods=['GET'])
-def get_saved_lists():
-    conn = sqlite3.connect('shopping_list.db')
-    c = conn.cursor()
-
-    # Fetch all lists from the database
-    c.execute("SELECT id, list_data, created_at FROM lists")
-    rows = c.fetchall()
-
-    lists = []
-    for row in rows:
-        list_dict = {
-            'id': row[0],
-            'list_data': row[1],
-            'created_at': row[2]
-        }
-        lists.append(list_dict)
-
-    conn.close()
-    return jsonify(lists)
 
 @fetch_list_bp.route('/fetch_list_items', methods=['POST'])
 def fetch_list_items():
